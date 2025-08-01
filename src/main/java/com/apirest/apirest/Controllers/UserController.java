62849e7c85b306b12cb5e248917472693a85db57
francellaCC <102675185+francellaCC@users.noreply.github.com>
2025-07-31 20:01:42 -0600

    feat: add conection with frontend

--- src/main/java/com/apirest/apirest/Controllers/UserController.java
+++ src/main/java/com/apirest/apirest/Controllers/UserController.java
@@ -1,4 +1,28 @@
 package com.apirest.apirest.Controllers;
 
+import com.apirest.apirest.Entities.User;
+import com.apirest.apirest.services.UserService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+    @PostMapping("/register")
+    public ResponseEntity<?> create(@RequestBody User user, @AuthenticationPrincipal Jwt jwt){
+        String email =  jwt.getClaimAsString("email");
+        System.err.println(email);
+        System.err.println(user);
+        return ResponseEntity.ok(userService.createUser(email, user));
+    }
 }
--- src/main/java/com/apirest/apirest/Repositories/UserRepository.java
+++ src/main/java/com/apirest/apirest/Repositories/UserRepository.java
@@ -1,4 +1,11 @@
 package com.apirest.apirest.Repositories;
 
-public interface UserRepository {
+import com.apirest.apirest.Entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findUserById(Long id);
+    Optional<User> findByEmail(String email);
 }
