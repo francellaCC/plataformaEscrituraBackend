852895aa7632c12841738ca3c1215ce72d64ed38
francellaCC <102675185+francellaCC@users.noreply.github.com>
2025-08-12 20:19:51 -0600

    feat: add response and request page dtos and pageRepository

---
+++ src/main/java/com/apirest/apirest/Dtos/PageRequestDTO.java
@@ -0,0 +1,29 @@
+package com.apirest.apirest.Dtos;
+
+public class PageRequestDTO {
+
+   private String content;
+   private Integer pageNumber;
+
+   public PageRequestDTO(String content, Integer pageNumber) {
+      this.content = content;
+      this.pageNumber = pageNumber;
+   }
+
+   public String getContent() {
+      return content;
+   }
+
+   public void setContent(String content) {
+      this.content = content;
+   }
+
+   public Integer getPageNumber() {
+      return pageNumber;
+   }
+
+   public void setPageNumber(Integer pageNumber) {
+      this.pageNumber = pageNumber;
+   }
+
+}
